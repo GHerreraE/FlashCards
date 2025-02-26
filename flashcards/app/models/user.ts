767f116d60ea30caf71371a55e0ef93b2de92def
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-02-26 13:22:18 +0100

    feat(user.ts) : Changement de critère
    [10][Done]
    Changement du type d'input -> Email to Username pour le formulaire login

--- flashcards/app/models/user.ts
+++ flashcards/app/models/user.ts
@@ -1,30 +1,39 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeSave, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
-  uids: ['email'],
+  uids: ['username'],
   passwordColumnName: 'password',
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
+  public static table = 't_user'
+
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-  declare fullName: string | null
-
-  @column()
-  declare email: string
+  declare username: string
 
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  declare isAdmin: boolean
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @beforeSave()
+  static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await hash.use('scrypt').make(user.password)
+    }
+  }
 }
