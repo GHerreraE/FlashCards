9aa110e3942a171e11aad1b5a2a7caa4cddff410
Gonzalo Herrera <gonzajavi2110@gmail.com>
2025-03-14 22:19:34 +0100

    feat(routes.ts): Création de route pour le post de decks

--- flashcards/start/routes.ts
+++ flashcards/start/routes.ts
@@ -1,19 +1,23 @@
+import router from '@adonisjs/core/services/router'
 import AuthController from '../app/controllers/auth_controller.js'
 import RegisterController from '#controllers/registers_controller'
-import router from '@adonisjs/core/services/router'
+import AuthMiddleware from '#middleware/auth_middleware'
 
-// Affichage du formulaire d'inscription (GET)
+// Routes accessibles sans authentification
 router.get('/register', [RegisterController, 'showRegister']).as('showRegister')
-
-// Traitement de l'inscription (POST)
 router.post('/register', [RegisterController, 'register']).as('handleRegister')
-
-router.on('/home').render('home').as('home')
 router.on('/login').render('login').as('login')
+router.post('/login', [AuthController, 'login'])
+router.post('/logout', [AuthController, 'logout']).as('logout')
+router.on('/').redirect('home')
+router.on('/home').render('home').as('home')
 router.on('/apropos').render('apropos').as('apropos')
 router.on('/contact').render('contact').as('contact')
 router.on('/homeuser').render('homeuser').as('homeuser')
-router.on('/').redirect('home')
-router.post('/login', [AuthController, 'login'])
-router.post('/logout', [AuthController, 'logout']).as('logout')
-router.post('/createdeck', 'DeckController.create').as('createDeck')
+
+// Groupe de routes nécessitant l'authentification
+router
+  .group(() => {
+    router.post('/create-deck', 'DecksController.createDeck').as('createDeck')
+  })
+  .middleware([() => new AuthMiddleware()])
