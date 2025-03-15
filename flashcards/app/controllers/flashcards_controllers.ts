a26d52e2c46203da99a7d234f8d1342a31b41de8
Gonzalo Herrera <gonzajavi2110@gmail.com>
2025-03-15 16:19:50 +0100

    feat(flashcards_controllers.ts): Création de nouveau controleur
    [20][Done]
    Création de nouveau controleur pour aficher les decks et créer

---
+++ flashcards/app/controllers/flashcards_controllers.ts
@@ -0,0 +1,30 @@
+// app/Controllers/Http/FlashcardsController.ts
+import Flashcard from '#models/flashcards'
+import Deck from '#models/decks'
+import { HttpContext } from '@adonisjs/core/http'
+
+export default class FlashcardsController {
+  public async store({ request, response, params }: HttpContext) {
+    const { question, answer } = request.only(['question', 'answer'])
+    const deck = await Deck.findOrFail(params.deckId)
+
+    const flashcard = await Flashcard.create({
+      question,
+      answer,
+      deckId: deck.id, // Associate flashcard with the deck
+    })
+
+    return response.redirect().toRoute('decks.show', { id: deck.id }) // Redirect to the deck's page after adding flashcard
+  }
+
+  public async index({ params, response, view }: HttpContext) {
+    try {
+      const deckId = params.id
+      const flashcards = await Flashcard.query().where('deck_id', deckId) // Get flashcards for this deck
+
+      return view.render('decks/show', { flashcards, deckId })
+    } catch (error) {
+      return response.status(500).json({ message: 'Error retrieving flashcards' })
+    }
+  }
+}
