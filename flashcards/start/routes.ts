0dcb1bf06d5d44b12864b10f85b0c2ea4d1ad0c9
Gonzalo Herrera <gonzajavi2110@gmail.com>
2025-03-15 16:30:43 +0100

    feat(routes.ts): Nouvelles routes
    [20][Done]
    Création et modification de nouvelles routes pour la gestion des pages/requetes des decks/flascards

--- flashcards/start/routes.ts
+++ flashcards/start/routes.ts
@@ -2,6 +2,8 @@ import router from '@adonisjs/core/services/router'
 import AuthController from '../app/controllers/auth_controller.js'
 import RegisterController from '#controllers/registers_controller'
 import AuthMiddleware from '#middleware/auth_middleware'
+import DecksController from '#controllers/decks_controllers'
+import FlashcardsController from '#controllers/flashcards_controllers'
 
 // Routes accessibles sans authentification
 router.get('/register', [RegisterController, 'showRegister']).as('showRegister')
@@ -13,7 +15,21 @@ router.on('/').redirect('home')
 router.on('/home').render('home').as('home')
 router.on('/apropos').render('apropos').as('apropos')
 router.on('/contact').render('contact').as('contact')
-router.on('/homeuser').render('homeuser').as('homeuser')
+router.get('/homeuser', [DecksController, 'index'])
+
+// Deck routes
+router.get('/decks', [DecksController, 'index'])
+router.get('/decks/create', [DecksController, 'create'])
+router.post('/decks', [DecksController, 'store']).as('decks.store')
+router.get('/decks/:id', [DecksController, 'show']).as('decks.show') // Show deck with options to edit, delete, and add flashcards
+router.get('/decks/:id/edit', [DecksController, 'edit']).as('decks.edit') // Show the edit page
+router.put('/decks/:id', [DecksController, 'update']).as('decks.update') // Update deck
+
+// Delete deck route
+router.delete('/decks/:id', [DecksController, 'destroy']).as('decks.destroy') // Delete deck
+
+// Flashcard routes
+router.post('/decks/:deckId/flashcards', [FlashcardsController, 'store']).as('flashcards.store')
 
 // Groupe de routes nécessitant l'authentification
 router
